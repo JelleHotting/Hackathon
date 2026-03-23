c4d11cc1ce434275b5b3dd9a9be45d520033950c
ClaireOfSoftware <ClaireOfSoftware@proton.me>
2026-03-23 22:04:42 +0100

    add: particles
    #1

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -19,9 +19,48 @@ const mouse = {
 window.addEventListener('mousemove', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
-    console.log(mouse.x, mouse.y);
+    // console.log(mouse.x, mouse.y);
 })
 
 ctx.fillStyle = 'white';
 ctx.font = '30px Verdana'; // <---- Here you can change the font size and family
-ctx.fillText('A', 0, 40) // <---- Here you can change the content
+ctx.fillText('A', 0, 30) // <---- Here you can change the content
+// ctx.strokeStyle = 'white';
+// ctx.strokeRect (0, 0, 100, 100);
+const data = ctx.getImageData (0, 0, 100, 100);
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = 3;
+        this.baseX = this.x
+        this.baseY = this.y
+        this.density = (Math.random() * 30) + 1;
+    }
+    draw(){
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+function init () {
+    particleArray = [];
+    particleArray.push(new Particle(50, 50));
+    particleArray.push(new Particle(80, 50));
+}
+
+init();
+console.log(particleArray);
+
+function animate () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particleArray.length; i++) {
+        particleArray[i].draw();
+        requestAnimationFrame(animate);
+    }
+}
+animate();
