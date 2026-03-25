5daa4e199cb65cbd802b742c171e1c2031f6f20a
Justin <justin.admiraal@hva.nl>
2026-03-25 13:52:46 +0100

    Gewerkt aan scrollanimatie met gsap
    Met scrollanimaties in gsap de informatie weergeven van nebula.

---
+++ scrollAnimation.js
@@ -0,0 +1,128 @@
+/* Gebruikte bronnen van gsap documentatie:
+https://gsap.com/docs/v3/
+https://gsap.com/docs/v3/Plugins/ScrollTrigger/
+https://gsap.com/docs/v3/Plugins/ScrollSmoother/
+https://gsap.com/docs/v3/Plugins/SplitText/
+https://gsap.com/docs/v3/GSAP/gsap.matchMedia()/#accessible-animations-with-prefers-reduced-motion
+*/
+
+// GSAP plugins registeren
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin, SplitText, ScrollSmoother);
+
+// Smoothscroll aanzetten
+ScrollSmoother.create({
+  wrapper: "#smooth-wrapper",
+  content: "#smooth-content",
+  smooth: 1,
+  effects: true,
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Animaties uit als reduced motion aan staat
+  const prefersReducedMotion = window.matchMedia(
+    "(prefers-reduced-motion: reduce)",
+  ).matches;
+
+  if (!prefersReducedMotion) {
+    // Animatie eerste sectie
+    gsap.from(".introSection .informationText", {
+      scrollTrigger: {
+        trigger: ".introSection",
+        start: "top center",
+        end: "top 150px",
+        scrub: 1.5,
+      },
+      rotation: 180,
+      x: -200,
+      opacity: 0,
+      scale: 0.2,
+    });
+
+    // Animatie tweede sectie
+    gsap.from(".missionSection .informationText", {
+      scrollTrigger: {
+        trigger: ".missionSection",
+        start: "top center",
+        end: "top 50px",
+        scrub: 1.5,
+      },
+      rotation: 360,
+      x: 200,
+      opacity: 0,
+      scale: 0.2,
+    });
+
+    // Animatie derde sectie
+    gsap.from(".corevalueSection .informationText", {
+      scrollTrigger: {
+        trigger: ".corevalueSection",
+        start: "top center",
+        end: "top 150px",
+        scrub: 2.5,
+      },
+      rotation: 180,
+      x: -400,
+      opacity: 0,
+      scale: 0.2,
+    });
+
+    // Satteliet svg wat het pad volgt
+    gsap.to(".satellite", {
+      opacity: 1,
+      immediateRender: true,
+      motionPath: {
+        path: "#guide-path",
+        align: "#guide-path",
+        alignOrigin: [0.5, 0.5],
+        autoRotate: true,
+        useSVG: true,
+      },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".informationSectionsWrapper",
+        start: "top top",
+        end: "bottom 20%",
+        scrub: 3,
+        markers: true,
+      },
+    });
+
+    // Horizontal scroll container
+    const horizontalScroll = gsap.to(".horizontalContainer", {
+      x: () =>
+        -(
+          document.querySelector(".horizontalContainer").scrollWidth -
+          window.innerWidth
+        ),
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".horizontalContainer",
+        start: "top top",
+        end: () =>
+          "+=" + document.querySelector(".horizontalContainer").scrollWidth,
+        pin: true,
+        scrub: 1,
+        markers: true,
+      },
+    });
+
+    // Text fade in animatie
+    document.fonts.ready.then(() => {
+      gsap.utils.toArray(".panel h2, .panel p").forEach((el) => {
+        const split = SplitText.create(el, { type: "words" });
+        gsap.from(split.words, {
+          opacity: 0,
+          duration: 0.5,
+          ease: "sine.out",
+          stagger: 0.05,
+          scrollTrigger: {
+            trigger: el,
+            containerAnimation: horizontalScroll,
+            toggleActions: "play none none reset",
+            start: "left 80%",
+          },
+        });
+      });
+    });
+  }
+});
